05336eea4912d1a76716a5db53dd695fe0e4c32a
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2021-11-09 16:51:08 +0400

    js https

--- resources/js/Components/Pagination/Pagination.vue
+++ resources/js/Components/Pagination/Pagination.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="pagination flex center">
-        <inertia-link v-if="prev_page_url" :href="prev_page_url">
+        <a v-if="prev_page_url" :href="prev_page_url">
             <button class="arr"><img src="/landing_resources/img/icons/pag/prev.svg" alt=""/></button>
-        </inertia-link>
+        </a>
         <template
             v-for="(link, index) in links.slice(1, -1)"
             :key="index"
         >
 
-            <inertia-link :href="link.url">
+            <a :href="link.url">
                 <button
                     class="num"
                     :class="link.active ? 'active' : ''"
                 >
                     {{ link.label }}
                 </button>
-            </inertia-link>
+            </a>
         </template>
-        <inertia-link v-if="next_page_url" :href="next_page_url">
+        <a v-if="next_page_url" :href="next_page_url">
 
             <button class="arr"><img src="/landing_resources/img/icons/pag/next.svg" alt=""/></button>
-        </inertia-link>
+        </a>
 
     </div>
 </template>
